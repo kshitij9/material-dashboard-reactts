4c2b51f39d6356baee412f78acb9274d08aa8698
Kanishk Sankpal <kanishkelliot@gmail.com>
2021-02-24 17:22:44 +0530

    added appbar and drawer

--- src/index.tsx
+++ src/index.tsx
@@ -4,9 +4,10 @@ import { createBrowserHistory } from 'history';
 import { Router, Route, Switch, Redirect } from 'react-router-dom';
 
 // core components
-import Admin from './layouts/Admin';
-import RTL from './layouts/RTL';
+// import Admin from './layouts/Admin';
+// import RTL from './layouts/RTL';
 import Forms from './layouts/forms';
+import coursesHome from './layouts/courses-home';
 
 import 'assets/css/material-dashboard-react.css?v=1.6.0';
 
@@ -16,10 +17,11 @@ ReactDOM.render(
 // tslint:disable-next-line: jsx-wrap-multiline
   <Router history={hist}>
     <Switch>
-      <Route path="/admin" component={Admin} />
-      <Route path="/rtl" component={RTL} />
+      {/* <Route path="/admin" component={Admin} /> */}
+      {/* <Route path="/rtl" component={RTL} /> */}
       <Route path="/forms" component={Forms} />
-      <Redirect from="/" to="/admin/dashboard" />
+      <Route path='/dbLearningAI/home' component={coursesHome}/>
+      <Redirect from="/" to="/dbLearningAI/home" />
     </Switch>
   </Router>,
   document.getElementById('root')
